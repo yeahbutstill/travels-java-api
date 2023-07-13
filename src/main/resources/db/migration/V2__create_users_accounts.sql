1534e92af1699093dc72f21ea48f4d17c4b655d2
yeahbutstill <mayadani28@gmail.com>
2023-07-13 13:34:48 +0700

    insert user

--- src/main/resources/db/migration/V2__create_users_accounts.sql
+++ src/main/resources/db/migration/V2__create_users_accounts.sql
@@ -20,7 +20,8 @@ primary key(id),
 foreign key(user_id) references users(id),
 foreign key(account_id) references accounts(id));
 
-insert into users(name, email) values ('swagger user', 'dev@swagger.user');
+insert into users(name, email, password, role) values ('swagger user', 'dev@swagger.user', 'password', 'ROLE_USER');
+insert into users(name, email, password, role) values ('Yeah But Still', 'dani@dani.com', 'rahasia', 'ROLE_ADMIN');
 
 alter table travel add column account_id integer;
 alter table travel add constraint fk_account_id foreign key (account_id) references accounts(id);
